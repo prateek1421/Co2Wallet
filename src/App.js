b951a55b832194d374b88048caaf5bd342914e0b
Prateek Srivastava <prateek.srivastava14@gmail.com>
2019-10-06 23:04:46 +0530

    xyz changes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Just here learning</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. prateek
         </p>
         <a
           className="App-link"
